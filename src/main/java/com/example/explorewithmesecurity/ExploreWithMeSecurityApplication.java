e3954ab67a5adb93115b4d3806125868200dd69c
BaFon77 <antocha-spartak@yandex.ru>
2023-08-17 18:53:56 +0300

    Initial commit

---
+++ src/main/java/com/example/explorewithmesecurity/ExploreWithMeSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.explorewithmesecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreWithMeSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExploreWithMeSecurityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/explorewithmesecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.example.explorewithmesecurity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
